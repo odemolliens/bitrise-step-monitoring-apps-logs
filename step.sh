ee53624ead366c34dd433a35b00b005c716f20be
Imran Mentese <imran.mentese@arhs-spikeseed.com>
2021-03-24 12:51:51 +0100

    imrovements

--- step.sh
+++ step.sh
@@ -7,7 +7,7 @@ if [[ ${check_android} == "yes" ]]; then
         exit 1
     fi
 
-    COUNT_ANDROID_LOGS=$(echo $(grep -ri "Landroid/util/Log;->v(\|Landroid/util/Log;->i(\|Landroid/util/Log;->w(\|Landroid/util/Log;->d(\|Landroid/util/Log;->e(" apk_decompiled/. | wc -l))
+    COUNT_ANDROID_LOGS=$(echo $(grep -ri "Landroid/util/Log;->v(\|Landroid/util/Log;->w(\|Landroid/util/Log;->d(\|Landroid/util/Log;->e(" apk_decompiled/. | wc -l))
 fi
 
 echo "---- REPORT ----"
@@ -18,18 +18,11 @@ fi
 
 printf ">>>>>>>>>>  APP LOGS  <<<<<<<<<<\n" >> quality_report.txt
 
-if [[ ${COUNT_ANDROID_LOGS} == "" && ${COUNT_IOS_LOGS} == "" ]]; then
+if [[ ${COUNT_ANDROID_LOGS} == "" ]]; then
     printf "0 log in your native code \n" >> quality_report.txt
 else
-    if [[ ${COUNT_ANDROID_LOGS} != "" ]]; then
-        if [ ${COUNT_ANDROID_LOGS} -gt "0" ]; then
-                printf "You have : $COUNT_ANDROID_LOGS logs in your Android code \n" >> quality_report.txt
-        fi
-    fi
-    if [[ ${COUNT_IOS_LOGS} != "" ]]; then
-        if [ ${COUNT_IOS_LOGS} -gt "0" ]; then
-                printf "You have : $COUNT_IOS_LOGS logs in your iOS code \n" >> quality_report.txt
-        fi
+    if [[ ${COUNT_ANDROID_LOGS} != "" && ${COUNT_ANDROID_LOGS} -gt "0" ]]; then
+        printf "You have : $COUNT_ANDROID_LOGS logs in your Android code \n" >> quality_report.txt
     fi
 fi
 
@@ -37,7 +30,7 @@ printf "\n\n" >> quality_report.txt
 
 cp quality_report.txt /Users/vagrant/deploy/quality_report.txt || true
 
-if [[ ${COUNT_ANDROID_LOGS} != "" && ${COUNT_ANDROID_LOGS} -gr "0" || ${COUNT_IOS_LOGS} != "" && ${COUNT_IOS_LOGS} -gr "0" ]]; then
+if [[ ${COUNT_ANDROID_LOGS} != "" && ${COUNT_ANDROID_LOGS} -gt "0" ]]; then
     echo "Generate an error due to logs in your native codes"
     exit 1
 fi
